5de153dde1bf2201da32f84f30b87aec43c3d573
WarWolf89 <almasi.csaba89@gmail.com>
2016-11-24 15:03:13 +0100

    beginning of multiplicative persistance excercise, converting int to string

---
+++ IdeaProjects/Codewars01/src/CodeWars1.java
@@ -0,0 +1,14 @@
+/**
+ * Created by almasics on 2016.11.24..
+ */
+public class CodeWars1 {
+    public static void main(String[] args) {
+        CodeWars1.persistence(4567);
+
+    }
+
+    public static void persistence(long n) {
+        String numberString = String.valueOf(n);
+        System.out.println(numberString);
+    }
+}
