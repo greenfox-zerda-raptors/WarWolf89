0cc9815cab402689c7c9e121b4ab5ca9d0b597db
WarWolf89 <almasi.csaba89@gmail.com>
2016-11-28 12:30:36 +0100

    starting the project, initial setup

---
+++ IdeaProjects/Codewars02/src/Consecutives.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Consecutives {
+
+    List<Integer> i = Arrays.asList(1,4,4,4,0,4,3,3,1);
+
+    public static void main(String[] args) {
+        sumConsecutives();
+    }
+
+    public static List<Integer> sumConsecutives(List<Integer> s) {
+    return null;
+    }
+}
