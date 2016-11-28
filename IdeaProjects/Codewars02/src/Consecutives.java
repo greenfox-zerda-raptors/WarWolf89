2d3ffb9c9d29e280c2ca87a4e4ca2ef14266e2eb
WarWolf89 <almasi.csaba89@gmail.com>
2016-11-28 14:38:45 +0100

    starting to take shape, first three numbers are correct, rest isno good

--- IdeaProjects/Codewars02/src/Consecutives.java
+++ IdeaProjects/Codewars02/src/Consecutives.java
@@ -1,26 +1,30 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class Consecutives {
 
-    private static List<Integer> i = Arrays.asList(1,4,4,4,0,4,3,3,1);
+    private static List<Integer> i = Arrays.asList(1, 4, 4, 4, 0, 4, 3, 3, 1);
 
     public static void main(String[] args) {
         System.out.println(sumConsecutives(i));
     }
 
     public static List<Integer> sumConsecutives(List<Integer> s) {
+
+        ListIterator<Integer> iterator = i.listIterator();
         ArrayList<Integer> sumOfNumbers = new ArrayList<>();
-        for (int j=1; j<i.size(); j++) {
+
+        for (int j = 1; j < i.size(); j++) {
             Integer numberOne = i.get(j-1);
             Integer numberTwo = i.get(j);
-            if (numberOne == numberTwo){
-                Integer sum = numberOne + numberTwo;
-                sumOfNumbers.add(sum);
+            if (numberOne == numberTwo || numberTwo == iterator.next()) {
+                Integer sum_2 = numberOne + numberTwo + iterator.next();
+                sumOfNumbers.add(sum_2);
+            }
+            else{
+                sumOfNumbers.add(numberOne);
+                sumOfNumbers.add(numberTwo);
             }
         }
-       return sumOfNumbers;
+        return sumOfNumbers;
     }
 }
