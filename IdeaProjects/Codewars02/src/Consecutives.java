0807bd3b0ba6c10aff7914669b51ab644b2f6caa
WarWolf89 <almasi.csaba89@gmail.com>
2016-11-28 12:59:36 +0100

    a functional loop, not quite the task, but can build on this, continuing wit hiterator method

--- IdeaProjects/Codewars02/src/Consecutives.java
+++ IdeaProjects/Codewars02/src/Consecutives.java
@@ -1,15 +1,26 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 public class Consecutives {
 
-    List<Integer> i = Arrays.asList(1,4,4,4,0,4,3,3,1);
+    private static List<Integer> i = Arrays.asList(1,4,4,4,0,4,3,3,1);
 
     public static void main(String[] args) {
-        sumConsecutives();
+        System.out.println(sumConsecutives(i));
     }
 
     public static List<Integer> sumConsecutives(List<Integer> s) {
-    return null;
+        ArrayList<Integer> sumOfNumbers = new ArrayList<>();
+        for (int j=1; j<i.size(); j++) {
+            Integer numberOne = i.get(j-1);
+            Integer numberTwo = i.get(j);
+            if (numberOne == numberTwo){
+                Integer sum = numberOne + numberTwo;
+                sumOfNumbers.add(sum);
+            }
+        }
+       return sumOfNumbers;
     }
 }
