804f7cbb4e3f2a4fe8b5e5a0fbe77c4de3bd2d2d
WarWolf89 <almasi.csaba89@gmail.com>
2016-11-28 19:54:33 +0100

    end of day result

--- IdeaProjects/Codewars02/src/Consecutives.java
+++ IdeaProjects/Codewars02/src/Consecutives.java
@@ -4,33 +4,30 @@ import java.util.*;
 
 public class Consecutives {
 
-    private static List<Integer> i = Arrays.asList(1, 4, 4, 4, 0, 4, 3, 3, 1);
-
+    static List<Integer> i = Arrays.asList(1, 4, 4, 4, 0, 4, 3, 3,1);
     public static void main(String[] args) {
         System.out.println(sumConsecutives(i));
     }
 
     public static List<Integer> sumConsecutives(List<Integer> s) {
-
-        ArrayList<Integer> sumOfNumbers = new ArrayList<>();
         List<Integer> i = Arrays.asList(1, 4, 4, 4, 0, 4, 3, 3,1);
+        ArrayList<Integer> sumOfNumbers = new ArrayList<>();
         Integer firstNumber;
         Integer secondNumber;
-        Integer sumFirstAndSecond;
+        Integer sumNumbers;
 
         for (int j = 0; j < i.size()-1; j++) {
                 firstNumber = i.get(j);
                 secondNumber = i.get(j+1);
 
             if (firstNumber == secondNumber){
-                sumFirstAndSecond = firstNumber + secondNumber;
-                sumOfNumbers.add(sumFirstAndSecond);
+                sumNumbers= firstNumber + secondNumber;
+                sumOfNumbers.add(sumNumbers);
             }
 
             else {
                 sumOfNumbers.add(firstNumber);
-                sumOfNumbers.add(secondNumber);
-                }
+            }
         }
 
         sumOfNumbers.add(i.get(i.size() - 1));
