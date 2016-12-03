13905c2262811a5b0d040f0ea2ec87de816caae0
WarWolf89 <almasi.csaba89@gmail.com>
2016-12-03 17:09:20 +0100

    final, reworked solution, accepted by codewars

--- IdeaProjects/Codewars04/src/App.java
+++ IdeaProjects/Codewars04/src/App.java
@@ -4,39 +4,30 @@ import com.sun.xml.internal.ws.util.StringUtils;
  * Created by almasics on 2016.12.02..
  */
 public class App {
-    static String returnString="";
-    static String test = "abcd";
-    static String lowerCaseString="";
-    static String temp ="";
-
-
     public static void main(String[] args) {
 
-        System.out.println(App.accum(test));
-//        System.out.println(App.lowerCaseCharString(test));
+        System.out.println(App.accum("jsdkFlhdsjkf"));
     }
 
 
 
     public static String accum(String s) {
+        String returnString;
         StringBuilder builder = new StringBuilder();
-        for (int i = 0; i < test.length(); i++) {
-            builder.append(Character.toUpperCase(test.charAt(i)));
-            builder.append(App.lowerCaseCharString(test));
+        for (int i = 0; i < s.length(); i++) {
+            builder.append(Character.toUpperCase(s.charAt(i)));
+            builder.append(App.lowerCaseCharString(s.charAt(i),i));
             builder.append("-");
-        }
 
-            return builder.toString();
+        }
+        return builder.substring(0,builder.length()-1);
+
     }
 
-    private static String lowerCaseCharString(String anotherS){
-        String lowerCaseCharacter="";
-        for (int i = 0; i < test.length(); i++) {
-            for (int j = 0; j <= i; j++) {
-                lowerCaseString+= lowerCaseCharacter;
-                lowerCaseCharacter = Character.toString(test.charAt(i));
-            }
-
+    private static String lowerCaseCharString(Character charAt, int number){
+        String lowerCaseString="";
+        for (int i = 0; i < number; i++) {
+            lowerCaseString+=Character.toLowerCase(charAt);
         }
         return lowerCaseString;
     }
