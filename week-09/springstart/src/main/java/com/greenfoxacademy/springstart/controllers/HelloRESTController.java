eff9131b39291ebb8bfbcc583a7f244e60180328
WarWolf89 <almasi.csaba89@gmail.com>
2016-12-19 14:46:03 +0100

    atomic long finished

--- week-09/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-09/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -1,7 +1,10 @@
 package com.greenfoxacademy.springstart.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Created by almasics on 2016.12.19..
@@ -9,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloRESTController {
 
+    private final AtomicLong counter = new AtomicLong();
+
     @RequestMapping(value = "/greeting")
-    public Greeting greeting() {
-        Greeting greeting = new Greeting(1, "Hello,World");
+    public Greeting greeting(@RequestParam String name) {
+        Greeting greeting = new Greeting(counter.incrementAndGet(), name);
         return greeting;
 
     }
