ab8d3f2c05eaccdae26ec894b8d791a8ef037726
WarWolf89 <almasi.csaba89@gmail.com>
2016-12-19 16:06:40 +0100

    final version done

--- week-09/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
+++ week-09/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicLong;
 
 /**
@@ -13,10 +15,14 @@ import java.util.concurrent.atomic.AtomicLong;
 public class HelloRESTController {
 
     static final AtomicLong counter = new AtomicLong();
+    static ArrayList<String> hellos = new ArrayList<>(Arrays.asList("Mirëdita", "Ahalan", "Parev", "Zdravei", "Nei Ho", "Dobrý den", "Ahoj", "Goddag", "Goede dag, Hallo", "Hello", "Saluton", "Hei", "Bonjour",
+            "Guten Tag", "Gia'sou", "Aloha", "Shalom", "Namaste", "Namaste", "Jó napot", "Halló", "Helló", "Góðan daginn", "Halo", "Aksunai", "Qanuipit", "Dia dhuit",
+            "Salve", "Ciao", "Kon-nichiwa", "An-nyong Ha-se-yo", "Salvëte", "Ni hao", "Dzien' dobry", "Olá", "Bunã ziua", "Zdravstvuyte", "Hola", "Jambo", "Hujambo", "Hej",
+            "Sa-wat-dee", "Merhaba", "Selam", "Vitayu", "Xin chào", "Hylo", "Sut Mae", "Sholem Aleychem", "Sawubona"));
 
     @RequestMapping(value = "/greeting")
     public Greeting greeting(@RequestParam String name) {
-        Greeting greeting = new Greeting(counter.incrementAndGet(), String.format("Hello %s", name));
+        Greeting greeting = new Greeting(counter.incrementAndGet(), String.format("%s %s", hellos.get(2), name));
         return greeting;
 
     }
--- week-09/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
+++ week-09/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloWebController.java
@@ -5,6 +5,9 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import static com.greenfoxacademy.springstart.controllers.HelloRESTController.counter;
+import static com.greenfoxacademy.springstart.controllers.HelloRESTController.hellos;
+
 /**
  * Created by almasics on 2016.12.19..
  */
@@ -14,7 +17,8 @@ public class HelloWebController {
     @RequestMapping("/web/greeting")
     public String greeting(@RequestParam String name, Model model) {
         model.addAttribute("name", name);
-        model.addAttribute("counter", HelloRESTController.counter.incrementAndGet());
+        model.addAttribute("counter", counter.incrementAndGet());
+        model.addAttribute("hellos", hellos.get(2));
         return "greeting";
     }
 }
