649f271d3ec937791c36c915f73a9e02d8cfd586
WarWolf89 <almasi.csaba89@gmail.com>
2016-12-19 13:41:50 +0100

    excercise 3 upcoming, param request

---
+++ week-09/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/Greeting.java
@@ -0,0 +1,16 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * Created by almasics on 2016.12.19..
+ */
+@Getter
+@AllArgsConstructor
+
+public class Greeting {
+    long id;
+    String content;
+
+}
---
+++ week-09/springstart/src/main/java/com/greenfoxacademy/springstart/controllers/HelloRESTController.java
@@ -0,0 +1,18 @@
+package com.greenfoxacademy.springstart.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by almasics on 2016.12.19..
+ */
+@RestController
+public class HelloRESTController {
+
+    @RequestMapping(value = "/greeting")
+    public Greeting greeting() {
+        Greeting greeting = new Greeting(1, "Hello,World");
+        return greeting;
+
+    }
+}
