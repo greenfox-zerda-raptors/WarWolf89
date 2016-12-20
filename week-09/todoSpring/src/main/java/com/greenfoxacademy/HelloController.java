291733739780bbb3f001d63a7ef6506b9cab5ef9
WarWolf89 <almasi.csaba89@gmail.com>
2016-12-20 16:20:41 +0100

    default name and thymeleaf added

---
+++ week-09/todoSpring/src/main/java/com/greenfoxacademy/HelloController.java
@@ -0,0 +1,27 @@
+package com.greenfoxacademy;
+
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * Created by almasics on 2016.12.20..
+ */
+@Getter
+@Controller
+public class HelloController {
+
+    String name;
+
+    @ResponseBody
+    @RequestMapping("/helloworld")
+    public String greeting(Model model, @RequestParam(required = false, defaultValue = "Thymeleaf") String name) {
+        this.name = name;
+        model.addAttribute("name", name);
+        String greeting = String.format("Hello %s", name);
+        return greeting;
+    }
+}
