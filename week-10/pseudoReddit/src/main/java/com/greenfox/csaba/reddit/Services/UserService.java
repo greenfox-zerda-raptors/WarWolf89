e976499849e5bc15c71adbe4d51e858b3093f9fa
WarWolf89 <almasi.csaba89@gmail.com>
2017-01-05 16:57:55 +0100

    user database has been created, next step: multi-user login

--- week-10/pseudoReddit/src/main/java/com/greenfox/csaba/reddit/Services/UserService.java
+++ week-10/pseudoReddit/src/main/java/com/greenfox/csaba/reddit/Services/UserService.java
@@ -1,5 +1,7 @@
 package com.greenfox.csaba.reddit.Services;
 
+import com.greenfox.csaba.reddit.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -7,4 +9,24 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserService {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Iterable<User> list() {
+        return userRepository.findAll();
+    }
+
+    public User get(Long id) {
+        return userRepository.findOne(id);
+    }
+
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+
 }
--- week-10/pseudoReddit/src/main/java/com/greenfox/csaba/reddit/controllers/UserController.java
+++ week-10/pseudoReddit/src/main/java/com/greenfox/csaba/reddit/controllers/UserController.java
@@ -1,7 +1,37 @@
 package com.greenfox.csaba.reddit.controllers;
 
+import com.greenfox.csaba.reddit.Services.UserService;
+import com.greenfox.csaba.reddit.domain.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 /**
  * Created by almasics on 2017.01.05..
  */
+@Controller
 public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        super();
+        this.userService = userService;
+    }
+
+    @RequestMapping(value = "/signUp", method = RequestMethod.GET)
+    public String newUser(Model model) {
+        model.addAttribute(new User());
+        return "/signUp";
+    }
+
+    @RequestMapping(value = "/signUp", method = RequestMethod.POST)
+    public String addUser(@ModelAttribute User user) {
+        userService.save(user);
+        return "redirect:/";
+    }
 }
