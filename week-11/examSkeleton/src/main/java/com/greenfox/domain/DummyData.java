869b3e0ea59d4e9e64340dfc0f20d46361011c81
WarWolf89 <almasi.csaba89@gmail.com>
2017-01-10 21:16:41 +0100

    some minor date and thymeleaf practice

--- week-11/examSkeleton/src/main/java/com/greenfox/domain/DummyData.java
+++ week-11/examSkeleton/src/main/java/com/greenfox/domain/DummyData.java
@@ -1,6 +1,7 @@
 package com.greenfox.domain;
 
 import javax.persistence.*;
+import java.util.Calendar;
 
 /**
  * Created by almasics on 2017.01.09..
@@ -12,13 +13,12 @@ public class DummyData {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String data;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Calendar createdOn;
+
 
     public DummyData() {
-
-    }
-
-    public DummyData(String data) {
-        this.data = data;
+        this.createdOn = Calendar.getInstance();
     }
 
     public Long getId() {
@@ -35,5 +35,13 @@ public class DummyData {
 
     public void setData(String data) {
         this.data = data;
+    }
+
+    public Calendar getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Calendar createdOn) {
+        this.createdOn = createdOn;
     }
 }
